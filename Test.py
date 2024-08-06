3fed9c996c29e9facb9766bb2c2b1a66b3534610
krarktel112 <krarktel@gmail.com>
2024-08-06 14:30:30 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -16,7 +16,7 @@ def driver(request):
     driver = webdriver.Firefox(options=options, service=service)
     driver.set_window_size(1920, 1080)
     driver.maximize_window()
-    driver.get(www.facebook.com)
+    driver.get(https://www.facebook.com/login/identify/?ctx=recover&next=https%3A%2F%2Fwww.facebook.com%2Flogin%2Fidentify%2F%3Fctx%3Drecover%26ars%3Dfacebook_login%26from_login_screen%3D0&from_login_screen=0)
     driver.implicitly_wait(10)
 
     yield driver
