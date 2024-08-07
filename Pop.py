d9ecdd54e2b9d4c5d9c9877d68134b125b19ec05
krarktel112 <krarktel@gmail.com>
2024-08-07 12:03:54 -0400

    Update Pop.py

--- Pop.py
+++ Pop.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+browser = webdriver.Firefox() 
+browser.get('https://www.twitter.com')
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
