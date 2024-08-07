4d6e9d2f83a4dfa1c443e11e265e3e8acc8c4c33
krarktel112 <krarktel@gmail.com>
2024-08-07 11:59:27 -0400

    Update Pop.py

--- Pop.py
+++ Pop.py
@@ -1,28 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-"""usr=input('Enter Email Id:') """
-"""pwd=input('Enter Password:')"""
-x = 99999999
-"""Set up webdriver fixture."""
-options = Options()
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-service = Service(executable_path="firefox.geckodriver")
-driver = webdriver.Firefox(options=options, service=service)
-driver.set_window_size(1920, 1080)
-driver.maximize_window()
-driver.implicitly_wait(10)
-driver.get("https://www.facebook.com/login/web/?email=amschwab%40comcast.net&is_from_lara=1")
-driver.implicitly_wait(10)
-login_box = find_element(By.NAME, 'reset_action')
-login_box.click()
-driver.implicitly_wait(10)
-input = find_element(By.ID, 'recovery_code_entry')
-input = send_keys(x)
-
-driver.quit()
-print ("Done")
-input('Press anything to quit')
-print("Finished")
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close(
