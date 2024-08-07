4d8cfeba986e54b84ef4e360b95b3f204ee5620d
krarktel112 <krarktel@gmail.com>
2024-08-07 12:11:56 -0400

    Update Pop.py

--- Pop.py
+++ Pop.py
@@ -1,7 +1,6 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 opts = Options()
-opts.set_headless()
 assert opts.headless  # Operating in headless mode
 browser = Firefox(options=opts)
 browser.get('https://duckduckgo.com')
