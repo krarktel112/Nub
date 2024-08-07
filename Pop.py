4ff2ea54b44898faf7f4f3a257b312946aa5b7f2
krarktel112 <krarktel@gmail.com>
2024-08-07 11:24:27 -0400

    Update Pop.py

--- Pop.py
+++ Pop.py
@@ -1,24 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
-def driver(request):
-    """Set up webdriver fixture."""
-    options = Options()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+"""usr=input('Enter Email Id:') """
+"""pwd=input('Enter Password:')"""
+x = 99999999
+"""Set up webdriver fixture."""
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+service = Service(executable_path="firefox.geckodriver")
+driver = webdriver.Firefox(options=options, service=service)
+driver.set_window_size(1920, 1080)
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.get("https://www.facebook.com/login/web/?email=amschwab%40comcast.net&is_from_lara=1")
+driver.implicitly_wait(10)
+login_box = find_element(By.NAME, 'reset_action')
+login_box.click()
+driver.implicitly_wait(10)
+input = find_element(By.ID, 'recovery_code_entry')
+input = send_keys(x)
 
-    service = Service(executable_path="firefox.geckodriver")
-    driver = webdriver.Firefox(options=options, service=service)
-    driver.set_window_size(1920, 1080)
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-
-    yield driver
-    driver.quit()
-
-browser = webdriver.Firefox()
-browser.get("http://www.python.org")
-driver.close()
+yield driver
+driver.quit()
+print ("Done")
+input('Press anything to quit')
+print("Finished")
