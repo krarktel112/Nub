7169312048c82a2577479db01037f3fb76152277
krarktel112 <krarktel@gmail.com>
2024-08-07 12:09:47 -0400

    Update Pop.py

--- Pop.py
+++ Pop.py
@@ -1,15 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
-browser = webdriver.Firefox() 
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+opts = Options()
+opts.set_headless()
+assert opts.headless  # Operating in headless mode
+browser = Firefox(options=opts)
+browser.get('https://duckduckgo.com')
 browser.get('https://www.twitter.com')
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
