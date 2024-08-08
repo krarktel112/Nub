48efb3495d89cc3677eba603b52f3874401740f0
krarktel112 <krarktel@gmail.com>
2024-08-08 14:52:06 -0400

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -2,7 +2,8 @@ import urllib.request
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
-
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 webUrl=urllib.request.urlopen('https://www.facebook.com/')
 
 htmldata=webUrl.read()
