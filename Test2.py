59b1411a5a1ac019c36cbe1236ad97787eb2fe7a
krarktel112 <krarktel@gmail.com>
2024-08-08 15:11:45 -0400

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -4,10 +4,11 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-System.setProperty("webdriver.chrome.driver","driver-location/chromedriver.exe");
+"""System.setProperty("webdriver.chrome.driver","driver-location/chromedriver.exe");"""
 
 webUrl=urllib.request.urlopen('https://www.facebook.com/')
-driver.getCurrentUrl();
+y = driver.getCurrentUrl();
 htmldata=webUrl.read()
 """print("result: "+str(webUrl.getCode()))"""
 print(htmldata)
+print (y)
