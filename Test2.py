4cbdcdfeeb91ad9696ee799de8af4d1ec8e7c09f
krarktel112 <krarktel@gmail.com>
2024-08-08 15:30:49 -0400

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -20,42 +20,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import codecs
 
-ser = Service(r"/data/data/com.turmux/files/home/chromedriver.exe")
-driver = webdriver.Chrome()
-
-driver.get("https://www.google.com")
-
-get_url = driver.current_url
-
-print("The current url is:"+str(get_url))
-
-#Redirect
-val = input("Enter a url: ")
-wait = WebDriverWait(driver, 10)
-driver.get(val)
-wait.until(EC.url_to_be(val))
-page_source = driver.page_source
-
-soup = BeautifulSoup(page_source,features="html.parser")
-title = soup.title.text
-file=codecs.open('article_titles.txt', 'a+')
-file.write(title+"\n")
-file.close()
-
-get_url = driver.current_url 
-print("The current url is:"+str(get_url))
-
-val = input("Enter a url: ")
-wait = WebDriverWait(driver, 10)
-driver.get(val)
-wait.until(EC.url_to_be(val))
-page_source = driver.page_source
-soup2 = BeautifulSoup(page_source,features="html.parser")
-title = soup2.title.text
-file=codecs.open('article_titles.txt', 'a+')
-file.write(str(title)+"\n")
-file.close()
-
-get_url = driver.current_url 
-print("The current url is:"+str(get_url))
-driver.quit()
+from selenium.webdriver.chrome.options import Options
+op = Options()
+op.binary_location('/usr/bin/chromium')
+op.add_argument('--no-sandbox')
