a0eadd500b7a87592d32ec831ed8aa1b8752ce75
krarktel112 <krarktel@gmail.com>
2024-08-10 14:18:07 -0400

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,3 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 title = driver.title
