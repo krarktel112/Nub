ecb9ff7d28474b7819bf012d17f2b4015894aca4
krarktel112 <krarktel@gmail.com>
2024-08-12 16:51:00 -0400

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -2,7 +2,7 @@ import asyncio
 from pyppeteer import launch
 
 async def scraper():
-   browser =await launch({"headless": False})
+   browser =await launch({"headless": True})
    page = await browser.newPage()
    await page.goto("https://www.scrapingcourse.com/ecommerce/")
 
