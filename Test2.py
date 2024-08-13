1cc86fc4548929c5dc60896e4d642d48073bf1ac
krarktel112 <krarktel@gmail.com>
2024-08-13 15:11:36 -0400

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,36 +1,13 @@
-import re
-import mechanize
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-br = mechanize.Browser()
-br.open("https//m.facebook.com/login/")
-# follow second link with element text matching regular expression
-response1 = br.follow_link(text_regex=r"cheese\s*shop", nr=1)
-print(br.title())
-print(response1.geturl())
-print(response1.info())  # headers
-print(response1.read())  # body
+# Make sure it is .exe file
+PATH = (r"C:/Users/deepa/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe")
+service = Service(executable_path=PATH) 
+driver = webdriver.Chrome(service=service)
 
-br.select_form(name="order")
-# Browser passes through unknown attributes (including methods)
-# to the selected HTMLForm.
-br["cheeses"] = ["mozzarella", "caerphilly"]  # (the method here is __setitem__)
-# Submit current form.  Browser calls .close() on the current response on
-# navigation, so this closes response1
-response2 = br.submit()
+# Open a website
+driver.get('https://www.wikipedia.com')
 
-# print currently selected form (don't call .submit() on this, use br.submit())
-print(br.form)
-
-response3 = br.back()  # back to cheese shop (same data as response1)
-# the history mechanism returns cached response objects
-# we can still use the response, even though it was .close()d
-response3.get_data()  # like .seek(0) followed by .read()
-response4 = br.reload()  # fetches from server
-
-for form in br.forms():
-    print(form)
-# .links() optionally accepts the keyword args of .follow_/.find_link()
-for link in br.links(url_regex="python.org"):
-    print(link)
-    br.follow_link(link)  # takes EITHER Link instance OR keyword args
-    br.back()
+# Close the browser
+driver.quit()
