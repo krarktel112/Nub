0956c8e6cbb0fd2383ea806b094f46f9bbaf0546
krarktel112 <krarktel@gmail.com>
2024-08-16 15:33:27 -0400

    Update Test4.py

--- Test4.py
+++ Test4.py
@@ -68,7 +68,7 @@ browser.submit()
 forms = list(browser.forms())
 form = forms[0]
 print(form)
-browser.select_form(nr=1)
+browser.select_form(nr=0)
 forms = list(browser.forms())
 form = forms[0]
 print(form)
