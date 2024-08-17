de23ef38bbc656615b4ad904573c8f6c88876d87
krarktel112 <krarktel@gmail.com>
2024-08-17 15:26:23 -0400

    Update Test4.py

--- Test4.py
+++ Test4.py
@@ -71,7 +71,7 @@ browser.select_form(nr=0)
 forms = list(browser.forms())
 form = forms[0]
 print(form)
-browser.form.set_value('999999', nr=0)
+browser.form.set_value('999999', nr=1)
 """browser.select_form(nr=0)"""
 """forms = list(browser.forms())"""
 """form = forms[0]"""
