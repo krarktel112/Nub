b853cb59f06aa3dac958676af8b74abc8fd6bf49
krarktel112 <krarktel@gmail.com>
2024-08-25 16:37:59 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -68,9 +68,9 @@ y = input('Continue? 1,2, else:')
 """reset code input"""
 browser.select_form(nr=0)
 check = browser.geturl()
-print check.read()      # the text of the page
+print(check.read())      # the text of the page
 response1 = browser.response()  # get the response again
-print response1.read()
+print(response1.read())
 soup = BeautifulSoup(response1.text, 'html.parser')
 titles = soup.find_all('h2')
 for title in titles:
