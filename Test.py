5c6bf60c10260ca0ad533ee0fcbc79f9df778d1e
krarktel112 <krarktel@gmail.com>
2024-08-31 12:48:43 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -60,33 +60,33 @@ def passcode8(pass0):
   pass1 = int(pass0)
   pass2 = int(pass0)
   """8"""
-  if pass1 < 10000000 and pass1 > 999999:
+  if pass1 < 10**7 and pass1 > 999999:
     pass2 = str(pass1)
     h = ""
     code = ("0", str(pass2))
     yo = h.join(code)
     """7"""
-  elif pass1 < 1000000 and pass1 > 99999:
+  elif pass1 < 10**6 and pass1 > 99999:
     pass2 = str(pass1)
     h = ""
     code = ("00", str(pass2))
     yo = h.join(code)
-  elif pass1 < 100000 and pass1 > 9999:
+  elif pass1 < 10**5 and pass1 > 9999:
     pass2 = str(pass1)
     h = ""
     code = ("000", str(pass2))
     yo = h.join(code)
-  elif pass1 < 10000 and pass1 > 999:
+  elif pass1 < 10**4 and pass1 > 999:
     pass2 = str(pass1)
     h = ""
     code = ("0000", str(pass2))
     yo = h.join(code)
-  elif pass1 < 1000 and pass1 > 99:
+  elif pass1 < 10**3 and pass1 > 99:
     pass2 = str(pass1)
     h = ""
     code = ("00000", str(pass2))
     yo = h.join(code)
-  elif pass1 < 100 and pass1 > 9:
+  elif pass1 < 10**2 and pass1 > 9:
     pass2 = str(pass1)
     h = ""
     code = ("000000", str(pass2))
