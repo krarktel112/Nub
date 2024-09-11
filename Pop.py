82b786eed6dadb18428b4a3ac946d8252de62944
krarktel112 <krarktel@gmail.com>
2024-09-11 17:42:33 -0400

    Delete Pop.py

--- Pop.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
- 
-#headless mode
-options = Options()
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-service = Service(executable_path="firefox.geckodriver")
-driver = webdriver.Firefox(options=options, service=service)
