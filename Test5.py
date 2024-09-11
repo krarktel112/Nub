849527020bb2a37c7f6a019900aab04de8beeca5
krarktel112 <krarktel@gmail.com>
2024-09-11 17:42:15 -0400

    Delete Test5.py

--- Test5.py
+++
@@ -1,4 +0,0 @@
-@client.event
-async def on_presence_update(before: discord.Member, after: discord.Member):
-    if after.id == my_Member_id:
-        print('{} changed status to {}'.format(after.display_name,after.status))
