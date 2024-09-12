c3c7442249a5d95a919a0418c699f63ad7e67258
krarktel112 <krarktel@gmail.com>
2024-09-12 00:18:18 -0400

    Update Passcoding.py

--- Passcoding.py
+++ Passcoding.py
@@ -1,9 +1,5 @@
 import itertools
 import sys
-from time import sleep
-from bs4 import BeautifulSoup
-import requests
-import mechanize
 import os
 
 os.system('clear')
@@ -17,5 +13,5 @@ for combination in itertools.product(range(10), repeat=8):
     f.write(''.join(map(str, combination)))
 z.close()
 print("Done 2")
-"""for combination in itertools.product([0,8], repeat=6):
+"""for combination in itertools.product([0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z], repeat=6):
     print(''.join(map(str, combination)))"""
