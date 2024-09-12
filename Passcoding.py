e9d85040ff632bbf05575e157840fb94f5586dc7
krarktel112 <krarktel@gmail.com>
2024-09-12 19:04:36 -0400

    Update Passcoding.py

--- Passcoding.py
+++ Passcoding.py
@@ -25,4 +25,4 @@ except:
     for combination in itertools.product(range(10), repeat=6):
         f.write(''.join(map(str, combination)))
         counter += 1
-        print(counter, end='\r')
+        print(counter)
