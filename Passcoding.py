36c913c5afcc5f925042b622c72ebaed86c7388e
krarktel112 <krarktel@gmail.com>
2024-09-13 00:12:55 -0400

    Update Passcoding.py

--- Passcoding.py
+++ Passcoding.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 os.system('clear')
-"""try:
+try:
     f = open("6digits.txt", "a")
     f.close()
 except:
@@ -18,12 +18,12 @@ except:
     for combination in itertools.product(range(10), repeat=6):
         z.write(''.join(map(str, combination)))
 counter = 0
-try:
+"""try:
     x = open("passing.txt", "a")
-except:"""
+except:
 x = open("passcoder.txt", "a+")
 counter = int(0)
 for combination in itertools.product(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","","X","Y","Z"], repeat=6):
     x.write(''.join(map(str, combination)))
     counter += 1
-    print(int(counter), end='\r')
+    print(int(counter), end='\r')"""
