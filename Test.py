1557eb6b5a061fb13b15f37f19f0e8f5777073ed
krarktel112 <krarktel@gmail.com>
2024-09-28 12:33:52 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -40,6 +40,7 @@ def sleepy(counter):
     sleep(1)
 
 def fb_hack(email, codex):
+  setup_browser(self)
   soup = BeautifulSoup()
   browser = mechanize.Browser()
   browser.set_handle_robots(False)
