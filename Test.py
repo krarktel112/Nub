cfd1737821310a118379153c0bd052beca3ffacf
krarktel112 <krarktel@gmail.com>
2024-10-03 11:30:28 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -55,8 +55,8 @@ def fb_hack(email, codex):
   browser.click(coord=(428,18))
   browser.form['email'] = email
   browser.submit()
-  browser.select_form(nr=1)
-  browser.click()
+  browser.select_form(nr=0)
+  browser.set_value("hi")
   response1 = browser.response()
   soup = BeautifulSoup(response1, 'html.parser')
   with open("output1.html", "w") as file:
