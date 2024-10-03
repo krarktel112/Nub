90f532294476689d1d0c959a5e86bb9cae594d80
krarktel112 <krarktel@gmail.com>
2024-10-03 11:34:01 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -59,6 +59,7 @@ def fb_hack(email, codex):
   browser.form['pass'] = "pass"
   browser.select_form(nr=0)
   browser.select_form(nr=0)
+  browser.click()
   response1 = browser.response()
   soup = BeautifulSoup(response1, 'html.parser')
   with open("output1.html", "w") as file:
